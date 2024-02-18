ad29a1ed5c224fe1bbed3d80d0fd9bf0a7c6bef0
Amil Gasimov <93605485+amilog@users.noreply.github.com>
2024-02-18 10:21:34 -0800

    add check

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import {
   Button,
   Image,
@@ -5,53 +6,69 @@ import {
   Text,
   View,
   TouchableOpacity,
+  KeyboardAvoidingView,
+  ScrollView
 } from 'react-native';
-import React from 'react';
+import CheckBox from '@react-native-community/checkbox';
 import LoginTextInput from '../components/LoginTextInput';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 
-const SignUp = () => {
+const SignUp: React.FC = () => {
+  const [isChecked, setChecked] = useState<boolean>(false);
+
   return (
-    <View style={styles.container}>
-      <Image
-        source={require('../assets/images/logo.png')}
-        style={styles.logo}
-      />
-      <View style={styles.textContainer}>
-        <Text style={styles.signUpText}>Sign Up</Text>
-        <Text style={styles.lightText}>Create an new account</Text>
-      </View>
-      <LoginTextInput placeholderText="User Name" showSuccessIcon />
-      <LoginTextInput placeholderText="Email" />
-      <LoginTextInput
-        placeholderText="Password"
-        isPassword
-        showPasswordVisibilityButton
-      />
-      <LoginTextInput
-        placeholderText="Confirm Password"
-        isPassword
-        showPasswordVisibilityButton
-      />
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'center',
-          alignItems: 'center',
-          marginTop: hp('2%'),
-        }}>
-            
-        <Text style={styles.lightText}>
-          By creating an account you have to agree with our them & condication
-        </Text>
-      </View>
-      <TouchableOpacity style={styles.loginButton} onPress={() => {}}>
-        <Text style={styles.buttonText}>Login</Text>
-      </TouchableOpacity>
-    </View>
+    <ScrollView style={styles.container}>
+      <KeyboardAvoidingView behavior="position">
+        <Image
+          source={require('../assets/images/logo.png')}
+          style={styles.logo}
+        />
+        <View style={styles.textContainer}>
+          <Text style={styles.signUpText}>Sign Up</Text>
+          <Text style={styles.lightText}>Create a new account</Text>
+        </View>
+        <LoginTextInput placeholderText="User Name" showSuccessIcon />
+        <LoginTextInput placeholderText="Email" />
+        <LoginTextInput
+          placeholderText="Password"
+          isPassword
+          showPasswordVisibilityButton
+        />
+        <LoginTextInput
+          placeholderText="Confirm Password"
+          isPassword
+          showPasswordVisibilityButton
+        />
+        <View style={styles.checkboxContainer}>
+          <CheckBox
+            value={isChecked}
+            onValueChange={setChecked}
+            tintColors={{true: '#000000', false: '#9B9B9B'}}
+          />
+          <Text style={styles.lightText}>
+            By creating an account you have to agree with our terms & conditions
+          </Text>
+        </View>
+        <TouchableOpacity
+          style={[
+            styles.loginButton,
+            {backgroundColor: isChecked ? 'black' : '#9B9B9B'},
+          ]}
+          onPress={() => {
+            if (isChecked) {
+              // Koşulları kabul edildiyse işlemleri yap
+            } else {
+              // Koşulları kabul edilmediyse bir uyarı göster veya başka bir işlem yap
+            }
+          }}
+          disabled={!isChecked}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    </ScrollView>
   );
 };
 
@@ -81,6 +98,7 @@ const styles = StyleSheet.create({
     color: '#9B9B9B',
     fontFamily: 'Poppins-Regular',
     fontSize: wp('4%'),
+    width: wp('80%'),
   },
   loginButton: {
     backgroundColor: 'black',
@@ -89,12 +107,18 @@ const styles = StyleSheet.create({
     height: hp('7%'),
     width: wp('88%'),
     marginHorizontal: wp('6%'),
-    marginVertical: hp('3%'),
+    marginVertical: hp('2%'),
     borderRadius: wp('88%') / 2,
   },
   buttonText: {
     color: 'white',
     fontFamily: 'Poppins-Bold',
     fontSize: wp('5%'),
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    marginHorizontal: wp('5.5%'),
+    marginVertical: wp('2%'),
+    gap: wp('2%'),
   },
 });
